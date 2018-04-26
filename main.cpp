88da9e86d17027cadb186c17a90d649764bbec46
Marko Jovanic <markoj@fidijaprojekt.co.rs>
2018-04-26 10:52:01 +0200

    third commit main

--- main.cpp
+++ main.cpp
@@ -1,29 +1,51 @@
 #include<iostream>
 #include"linkedList.cpp"
+#include"graph.cpp"
 using namespace std;
 
 int main()
 {
-	//linkedlist only
-	int cntr = 0; int index = 5;
-	while(1)
-	{
-		int* a = new int[10];
-		double* b = new double[10];
-		LinkedList<int>* first = new LinkedList<int>();
-		LinkedList<double>* second = new LinkedList<double>();
-		for(int i=0; i<10; i++)
-		{
-			a[i]=100*cntr+i; b[i]=100*cntr+i*i;
-			first->AddNode(a[i]); second->AddNode(b[i]);
-		}
-		cout << first->At(index)->data << endl;
-		delete(first); delete(second);
-		delete(a); delete(b);
-		cntr++;
-	}
-	// graph
-	
 
+	int cntr = 0; int index = 5; bool testLinkedListOnly = false;
+	if(testLinkedListOnly) 	//linkedlist only
+	{
+		while(1)
+		{
+			int* a = new int[10];
+			double* b = new double[10];
+			LinkedList<int>* first = new LinkedList<int>();
+			LinkedList<double>* second = new LinkedList<double>();
+			for(int i=0; i<10; i++)
+			{
+				a[i]=100*cntr+i; b[i]=100*cntr+i*i;
+				first->AddNode(a[i]); second->AddNode(b[i]);
+			}
+			cout << first->At(index)->data << endl;
+			delete(first); delete(second);
+			delete(a); delete(b);
+			cntr++;
+		}
+	}
+	else  // graph
+	{
+		while(1)
+		{
+			long* a = new long[10];
+			Graph<long>* myGraph = new Graph<long>();
+			for(int i=0; i<10; i++)
+			{
+				a[i]=10*cntr+i;
+				myGraph->AddNode(a[i]);
+				for(int j=0; j<i; j++)
+				{
+					if(i!=j)
+						myGraph->JoinNodes(i,j);
+				}
+			}
+			cout << myGraph->At(index)->data << endl;
+			delete [] a; delete myGraph;
+			cntr++;
+		}
+	}	
 	return 0;
 }
